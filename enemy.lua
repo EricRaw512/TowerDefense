a39ebab9b03c4af17628779391fd1479e173163e
EricRaw512 <ericraw512@gmail.com>
2023-05-30 16:02:30 +0700

    Attacking and Hp

--- enemy.lua
+++ enemy.lua
@@ -2,10 +2,12 @@
 
 Enemy = Entity:extend()
 
-function Enemy:new(x, y, width, height)
-    Enemy.super.new(self, x, y, width, height)
+function Enemy:new(x, y, width, height, hp, damage)
+    Enemy.super.new(self, x, y, width, height, hp, damage)
     self.speed = 100
     self.strength = 150
+    self.time = 0
+    self.damage = damage
 end
 
 function Enemy:update(dt)
@@ -21,4 +23,13 @@ end
 
 function Enemy:draw()
     love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
+
+function Enemy:attack(e, dt)
+    if self.time < 0 then
+        self.time = 3
+        e.hp = e.hp - self.damage
+    else
+        self.time = self.time - dt
+    end
 end
